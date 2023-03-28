07a18a55707554cdd23f9d6e4451f264f5c40a3a
Leandro Soto Cordova <127250759+LeandroSotoCordova@users.noreply.github.com>
2023-03-28 19:51:32 -0400

    Update Solver.py

--- Solver.py
+++ Solver.py
@@ -1,1 +1,14 @@
-
+class Lazors_Game:
+    def __init__(self, filename):
+        #Read file
+        self.file = open(filename, 'r').read()
+    
+    def raw_data_converter(self):
+        #Separate file's data into individual lines
+        data_lines = self.file.split('\n')
+        raw_data = [] #Store raw data
+        #Check every line of the data
+        for line in data_lines: 
+             if '#' not in line and line != '':
+                 #Store useful raw data
+                 raw_data.append(line)
